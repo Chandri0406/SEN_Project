406631541330902622285dee2782c5ae8882e215
DMCoder2022 <unidm2022@gmail.com>
2024-10-04 16:08:52 +0200

    Created code for Factory Design Pattern

---
+++ ApexCareSolutions/Models/Factory/CommercialContract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApexCareSolutions.Models.Factory
+{
+    public class CommercialContract : IContract, IResidency
+    {
+        int ContractID { get; set; }
+        int ClientID { get; set; }
+        DateTime StartDate { get; set; }
+        DateTime EndDate { get; set; }
+        string Status { get; set; }
+
+        string Type { get; set; }
+
+        string Residency { get; set; }
+
+        public CommercialContract(int clientID, DateTime startDate, DateTime endDate, string status, string residency)
+        {
+            ClientID = clientID;
+            StartDate = startDate;
+            EndDate = endDate;
+            Status = status;
+            Type = "Commercial";
+            Residency = residency;
+        }
+
+        public CommercialContract() 
+        {
+            Type = "Commercial";
+        }
+    }
+}
---
+++ ApexCareSolutions/Models/Factory/IResidency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexCareSolutions.Models.Factory
+{
+    public interface IResidency
+    {
+        string Residency { get; }
+    }
+}

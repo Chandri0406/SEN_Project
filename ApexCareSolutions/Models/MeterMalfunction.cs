557ee2e705b318fa9bb17c4b666ca9f3b4ca019a
Doglover26 <136690572+Doglover26@users.noreply.github.com>
2024-10-05 17:43:51 +0200

    Daniel
    Did Shit

---
+++ ApexCareSolutions/Models/MeterMalfunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApexCareSolutions.Models
+{
+    public class MeterMalfunction : Notifications
+    {
+        public MeterMalfunction() : base
+        {
+            
+        }
+
+        public string MeterID { get; set; }           // The identifier for the meter
+        public string IssueDescription { get; set; }  // Description of the malfunction or issue
+        public DateTime ReportedDate { get; set; }    // Date when the malfunction was detected/reported
+
+        // Constructor
+        public MeterMalfunctionNotification(int id, string message, DateTime dateSent, string recipient, string meterID, string issueDescription, DateTime reportedDate)
+            : base(id, message, dateSent, recipient)
+        {
+            MeterID = meterID;
+            IssueDescription = issueDescription;
+            ReportedDate = reportedDate;
+        }
+
+        // Method to send malfunction notification
+        public override void SendMalfunctionAlert()
+        {
+            // Logic to send the malfunction alert to the user
+            Console.WriteLine($"Meter {MeterID} has reported an issue: {IssueDescription} on {ReportedDate}. Please contact support for assistance.");
+        }
+    }
+}
---
+++ ApexCareSolutions/Models/Notifications.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApexCareSolutions.Models
+{
+    public abstract class Notifications
+    {
+
+        public Notifications()
+        {
+            
+        }
+
+        private int notificationID
+        private string message
+        private DateTime dateSent
+        private string recipient
+        private bool isRead
+
+
+        //Encapsualted fields:
+        public global::System.Int32 NotificationID { get => notificationID; set => notificationID = value; }
+        public global::System.String Message { get => message; set => message = value; }
+        public DateTime DateSent { get => dateSent; set => dateSent = value; }
+        public global::System.String Recipient { get => recipient; set => recipient = value; }
+        public global::System.Boolean IsRead { get => isRead; set => isRead = value; }
+
+        // Constructor
+        public Notification(int id, string message, DateTime dateSent, string recipient)
+        {
+            NotificationID = id;
+            Message = message;
+            DateSent = dateSent;
+            Recipient = recipient;
+            IsRead = false;
+        }
+
+        // Method to mark notification as read
+        public void MarkAsRead()
+        {
+            IsRead = true;
+        }
+    }
+
+    // Child Class: Contract Renewal
+    public class ContractRenewal : Notification
+    {
+        public int ContractID { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string RenewalOptions { get; set; }
+
+        // Constructor
+        public ContractRenewal(int id, string message, DateTime dateSent, string recipient, int contractID, DateTime expirationDate, string renewalOptions)
+            : base(id, message, dateSent, recipient)
+        {
+            ContractID = contractID;
+            ExpirationDate = expirationDate;
+            RenewalOptions = renewalOptions;
+        }
+
+        // Method to send renewal notification
+        public abstract void SendRenewalNotification();
+    }
+}

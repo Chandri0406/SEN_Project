80459892960ce0455bdab11c753a5d089db8d702
DmCoder2022 <unidm2022@gmail.com>
2024-10-05 16:32:43 +0200

    DBConnection Class created (unfinished)

---
+++ ApexCareSolutions/Models/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+
+namespace ApexCareSolutions.Models
+{
+    public class DBConnection
+    {
+        private readonly string _connectionString;
+
+        public DatabaseConnection()
+        {
+            _connectionString = $"Host=localhost;Database=ApexCare_DB;Username=Tester;Password=5432;";
+        }
+
+        public NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+
+    }
+}

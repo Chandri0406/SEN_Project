72be5accc20de888f2596d10ac38dc3b441ea422
Doglover26 <136690572+Doglover26@users.noreply.github.com>
2024-10-06 18:40:29 +0200

    Daniel
    promotional offers and contract renewals being pushed

--- ApexCareSolutions/Models/ContractRenewal.cs
+++ ApexCareSolutions/Models/ContractRenewal.cs
@@ -5,7 +5,32 @@ using System.Web;
 
 namespace ApexCareSolutions.Models
 {
-    public class ContractRenewal
+    public class ContractRenewal : Notifications
     {
+
+        public ContractRenewal() : base()
+        {
+            
+        }
+
+        public int ContractID { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string RenewalOptions { get; set; }
+
+        // Constructor
+        public ContractRenewal(int id, string message, DateTime dateSent, string recipient, int contractID, DateTime expirationDate, string renewalOptions)
+            : base(id, message, dateSent, recipient)
+        {
+            ContractID = contractID;
+            ExpirationDate = expirationDate;
+            RenewalOptions = renewalOptions;
+        }
+
+        // Method to send renewal notification
+        public override void SendRenewalNotification()
+        {
+            // Logic for sending notification
+            Console.WriteLine($"Contract {ContractID} is expiring on {ExpirationDate}. Options: {RenewalOptions}");
+        }
     }
 }
--- ApexCareSolutions/Models/PromotionalOffers.cs
+++ ApexCareSolutions/Models/PromotionalOffers.cs
@@ -5,7 +5,34 @@ using System.Web;
 
 namespace ApexCareSolutions.Models
 {
-    public class PromotionalOffers
+    public class PromotionalOffers : Notifications
     {
+
+        public PromotionalOffers() : base()
+        {
+            
+        }
+
+        public int OfferID { get; set; }
+        public string OfferDetails { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public double DiscountRate { get; set; }
+
+        // Constructor
+        public PromotionalOffers(int id, string message, DateTime dateSent, string recipient, int offerID, string offerDetails, DateTime expiryDate, double discountRate)
+            : base(id, message, dateSent, recipient)
+        {
+            OfferID = offerID;
+            OfferDetails = offerDetails;
+            ExpiryDate = expiryDate;
+            DiscountRate = discountRate;
+        }
+
+        // Method to send promotional offer
+        public override void SendOfferNotification()
+        {
+            // Logic for sending offer notification
+            Console.WriteLine($"Special offer {OfferID}: {OfferDetails}. Expiry: {ExpiryDate}. Discount: {DiscountRate}%");
+        }
     }
 }

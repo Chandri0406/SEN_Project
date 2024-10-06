4ef9a95b62594145bd548fdf4cd50937dbab5b78
UniLxrde <141034901+UniLxrde@users.noreply.github.com>
2024-10-06 17:16:45 +0200

    fixed

--- ApexCareSolutions/Models/Technician.cs
+++ ApexCareSolutions/Models/Technician.cs
@@ -1,29 +1,62 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
 
-namespace Assignment.Models
+namespace ApexCareSolutions.Models
 {
     public class Technician
     {
-        [Key]
-        public int TechnicianID { get; set; }
+        // Fields
+        private int TechnicianID;
+        private string Username;
+        private string FirstName;
+        private string LastName;
+        private string Phone;
+        private string Email;
 
-        [Required]
-        [StringLength(100)]
-        public string Username { get; set; }
+        // Constructor
+        public Technician(int technicianID, string username, string firstName, string lastName, string phone, string email)
+        {
+            TechnicianID = technicianID;
+            Username = username;
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+            Email = email;
+        }
 
-        [Required]
-        [StringLength(100)]
-        public string FirstName { get; set; }
+        // Properties
+        public int TechnicianID
+        {
+            get { return technicianID; }
+            set { technicianID = value; }
+        }
 
-        [Required]
-        [StringLength(100)]
-        public string LastName { get; set; }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
 
-        [StringLength(15)]
-        public string Phone { get; set; }
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
 
-        [EmailAddress]
-        [StringLength(100)]
-        public string Email { get; set; }
-    }
-}
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }

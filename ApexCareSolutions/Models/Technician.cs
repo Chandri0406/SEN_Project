f9c0e2323327bcd07d4def3ec0a531cdc032e96a
UniLxrde <141034901+UniLxrde@users.noreply.github.com>
2024-10-06 17:01:13 +0200

    Add files via upload

---
+++ ApexCareSolutions/Models/Technician.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment.Models
+{
+    public class Technician
+    {
+        [Key]
+        public int TechnicianID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [StringLength(15)]
+        public string Phone { get; set; }
+
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+    }
+}

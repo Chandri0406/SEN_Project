b99e52fde716e569c2cb7cbb69543046cdeba2a0
DMCoder2022 <unidm2022@gmail.com>
2024-10-06 15:22:35 +0200

    Added rest of project

---
+++ ApexCareSolutions/Models/DBConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ApexCareSolutions.Models.Factory;
+using Npgsql;
+
+namespace ApexCareSolutions.Models
+{
+    public class DBConnection
+    {
+        private readonly string _connectionString;
+        private ContractFactory contractFactory;
+        private IssueFactory issueFactory;
+
+        public DBConnection()
+        {
+            _connectionString = $"Host=localhost;Database=ApexCare_DB;Username=Tester;Password=5432;";
+        }
+
+        public NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+
+    }
+}
---
+++ ApexCareSolutions/Models/Factory/IContract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexCareSolutions.Models.Factory
+{
+    public interface IContract
+    {
+        int ContractID { get; }
+        int ClientID { get; }
+        DateTime StartDate { get; }
+        DateTime EndDate { get; }
+        string Status { get; }
+    }
+}

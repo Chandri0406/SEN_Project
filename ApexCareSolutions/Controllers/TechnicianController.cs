aeb39c79a8fade9562cd178c1d027a4bf49e6815
UniLxrde <141034901+UniLxrde@users.noreply.github.com>
2024-10-06 17:01:48 +0200

    Add files via upload

---
+++ ApexCareSolutions/Controllers/TechnicianController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Assignment.Models;
+using System.Linq;
+
+namespace Assignment.Controllers
+{
+    public class TechnicianController : Controller
+    {
+        private readonly
+    }
+}

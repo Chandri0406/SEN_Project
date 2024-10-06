98a156b8c776bbbdf5897471e615711a65873b5b
Marcel De Lange <marceldelange20@gmail.com>
2024-10-06 15:24:36 +0200

    Npgsql.dll installed , and client , calllog and serviceagent model code added

--- ApexCareSolutions/Models/Clients.cs
+++ ApexCareSolutions/Models/Clients.cs
@@ -7,5 +7,33 @@ namespace ApexCareSolutions.Models
 {
     public class Clients
     {
+        // UML Client Model 
+        //ClientID, Username, FirstName, LastName, Address, Phone, Email
+        public int ClientID { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+
+
+        public Clients()
+        {
+
+
+        }
+        public Clients(int clientID, string username, string firstName, string lastName, string address, string phone, string email)
+        {
+            ClientID = clientID;
+            Username = username;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            Phone = phone;
+            Email = email;
+        }
+
     }
 }
---
+++ ApexCareSolutions/Models/ServiceAgent.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApexCareSolutions.Models
+{
+    public class ServiceAgent
+    {
+        // UML ServiceAgentModel
+        // AgentID, Username, FirstName, LastName, Phone, Email
+        public string AgentID { get; set; }// int or string
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        // Constructor
+        public ServiceAgent() { }
+
+        // Constructor with parameters
+        public ServiceAgent(string agentID, string username, string firstName, string lastName, string phone, string email)
+        {
+            AgentID = agentID;
+            Username = username;
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+            Email = email;
+        }
+    }
+}

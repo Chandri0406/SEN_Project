402f3c8adac083b8b145db8567364a54ba96a99c
DmCoder2022 <unidm2022@gmail.com>
2024-10-12 18:36:14 +0200

    Added code to Contract History Page

--- ApexCareSolutions/Pages/Client/contractHistory.cshtml.cs
+++ ApexCareSolutions/Pages/Client/contractHistory.cshtml.cs
@@ -1,3 +1,4 @@
+using ApexCareSolutions.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,22 @@ namespace ApexCareSolutions.Pages.Client
 {
     public class contractHistoryModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Clients client { get; set; }
+        public PrivateContract contract { get; set; }
+        private ContractFactory cf { get; set; }
+
+        public void TestData()
         {
+            client.ClientID = 11;
+            contract = new PrivateContract(client.ClientID, DateTime.Now.Date, DateTime.Now.AddDays(10), "active", "apartment");
+        }
+
+        public IActionResult OnGet()
+        {
+            //To test the page
+            TestData();
+            return Page();
         }
     }
 }
--- ApexCareSolutions/obj/Debug/net8.0/ApexCareSolutions.AssemblyInfo.cs
+++ ApexCareSolutions/obj/Debug/net8.0/ApexCareSolutions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApexCareSolutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8082afef702455bb334b470d12f58c56b1f3b09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b697e0a4b6e50ed6fbe84db446f67a623f6a92dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApexCareSolutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApexCareSolutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

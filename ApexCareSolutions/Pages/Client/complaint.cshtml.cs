983c1d68df250bc1a1f5b3d0d41aef8f49428251
Peter <140653287+PeterSmal@users.noreply.github.com>
2024-10-12 23:10:58 +0200

    Added webpages for feedback and complaints
    still need to do the logic behind it

--- ApexCareSolutions/Pages/Client/complaint.cshtml.cs
+++ ApexCareSolutions/Pages/Client/complaint.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ApexCareSolutions.Models;
 
 namespace ApexCareSolutions.Pages.Client
 {
     public class complaintModel : PageModel
     {
+        public Complaint complaint { get; set; }
         public void OnGet()
         {
         }
--- ApexCareSolutions/Pages/Client/feedback.cshtml.cs
+++ ApexCareSolutions/Pages/Client/feedback.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ApexCareSolutions.Models;
 
 namespace ApexCareSolutions.Pages.Client
 {
     public class feedbackModel : PageModel
     {
+        public Feedback feedback { get; set; }
         public void OnGet()
         {
         }

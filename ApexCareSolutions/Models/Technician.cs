6b0c9afc0ea140f6c500eb47e0163891b2c89dfb
UniLxrde <141034901+UniLxrde@users.noreply.github.com>
2024-10-13 18:31:12 +0200

    Technician Updated

--- ApexCareSolutions/Models/Technician.cs
+++ ApexCareSolutions/Models/Technician.cs
@@ -6,64 +6,27 @@ namespace ApexCareSolutions.Models
     public class Technician
     {
         // Fields
-        private int TechnicianID;
-        private string Username;
-        private string FirstName;
-        private string LastName;
-        private string Phone;
-        private string Email;
+        public string TechnicianID { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public Technician()
+        {
+
+        }
 
         // Constructor
-        public Technician(int technicianID, string username, string firstName, string lastName, string phone, string email)
+        public Technician(string technicianID, string username, string firstname, string lastname, string phone, string email)
         {
             TechnicianID = technicianID;
             Username = username;
-            FirstName = firstName;
-            LastName = lastName;
+            FirstName = firstname;
+            LastName = lastname;
             Phone = phone;
             Email = email;
-        }
-
-        // Properties
-        public int TechnicianID
-        {
-            get { return technicianID; }
-            set { technicianID = value; }
-        }
-
-        public string Username
-        {
-            get { return username; }
-            set { username = value; }
-        }
-
-        public string FirstName
-        {
-            get { return firstName; }
-            set { firstName = value; }
-        }
-
-        public string LastName
-        {
-            get { return lastName; }
-            set { lastName = value; }
-        }
-
-        public string Phone
-        {
-            get { return phone; }
-            set { phone = value; }
-        }
-
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
-
-        public void ViewIssue(IIssue issue)
-        {
-            Console.WriteLine($"Handling issue ID: {issue.IssueID}, Priority: {issue.Priority}");
         }
     }
 }

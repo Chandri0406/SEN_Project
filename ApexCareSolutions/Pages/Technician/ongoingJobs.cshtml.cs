e2b601dcbb6facd73ce4a666bb83e01b701e404a
UniLxrde <141034901+UniLxrde@users.noreply.github.com>
2024-10-13 19:01:47 +0200

    Ongoing Jobs.cshtml.cs updated

--- ApexCareSolutions/Pages/Technician/ongoingJobs.cshtml.cs
+++ ApexCareSolutions/Pages/Technician/ongoingJobs.cshtml.cs
@@ -5,8 +5,31 @@ namespace ApexCareSolutions.Pages.Technician
 {
     public class ongoingJobsModel : PageModel
     {
+        public List<Technician> Technicians { get; set; }
+
+        [BindProperty]
+        public string clientUsername { get; set; }
+
+        [BindProperty]
+        public int IssueID { get; set; }
+
+        [BindProperty]
+        public int SelectedTechnicianID { get; set; }
+
+        [BindProperty]
+        public DateTime startDate { get; set; }
+
+        [BindProperty]
+        public DateTime endDate { get; set; }
+
         public void OnGet()
         {
+        
+        }
+
+        public IActionResult OnPost()
+        {
+            return Page();
         }
     }
 }

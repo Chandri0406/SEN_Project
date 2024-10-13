0bfcb955e4bdbc999873a58910cfb73a2933d60d
Chandri0406 <106561939+Chandri0406@users.noreply.github.com>
2024-10-13 15:37:50 +0200

    layouts

--- ApexCareSolutions/obj/Debug/net8.0/ApexCareSolutions.AssemblyInfo.cs
+++ ApexCareSolutions/obj/Debug/net8.0/ApexCareSolutions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApexCareSolutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7c64ae287d5bb41cb78e4b88e32dcad2134883")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+977b4f12908c36931e4f3e545bc95b3a3a56165b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApexCareSolutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApexCareSolutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

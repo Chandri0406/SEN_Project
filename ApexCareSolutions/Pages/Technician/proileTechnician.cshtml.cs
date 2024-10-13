428e97537d0f029af590e37084a3cac1f93bba79
UniLxrde <141034901+UniLxrde@users.noreply.github.com>
2024-10-13 19:24:22 +0200

    Updated proileTechnician

--- ApexCareSolutions/Pages/Technician/proileTechnician.cshtml.cs
+++ ApexCareSolutions/Pages/Technician/proileTechnician.cshtml.cs
@@ -5,6 +5,24 @@ namespace ApexCareSolutions.Pages.Technician
 {
     public class proileTechnicianModel : PageModel
     {
+        [BindProperty]
+        public string technicianID { get; set; }
+
+        [BindProperty]
+        public string UserName { get; set; }
+
+        [BindProperty]
+        public string firstName { get; set; }
+
+        [BindProperty]
+        public string lastName { get; set; }
+
+        [BindProperty]
+        public string Phone { get; set; }
+
+        [BindProperty]
+        public string Email { get; set; }
+
         public void OnGet()
         {
         }

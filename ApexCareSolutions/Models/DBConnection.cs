453184e9126c84b9807f3c8a724045d47b67c0d2
UniLxrde <141034901+UniLxrde@users.noreply.github.com>
2024-10-14 10:37:36 +0200

    GetTechnician from DBConnection added

--- ApexCareSolutions/Models/DBConnection.cs
+++ ApexCareSolutions/Models/DBConnection.cs
@@ -22,5 +22,40 @@ namespace ApexCareSolutions.Models
             return new NpgsqlConnection(_connectionString);
         }
 
+        //Using Stored Procedure for Getting Technicians
+        public List<profileTechnicianModel> GetTechnicians()
+        {
+            var technicians = new List<profileTechnicianModel>();
+
+            using (var conn = GetConnection()) 
+            {
+                conn.Open();
+
+                //Calling the Procedure
+                using (var command = new NpgsqlCommand("sp_gettchniciandetails(IN p_technicianid text)", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    //Executing Stored Procedure
+                    using (var reader = command.ExecuteReader()) 
+                    { 
+                        while (reader.Read())
+                        {
+                            var technician = new profileTechnicianModel
+                            {
+                                TechnicianID = reader.GetString(0),
+                                Username = reader.GetString(1),
+                                FirstName = reader.GetString(2),
+                                LastName = reader.GetString(3),
+                                Phone = reader.IsDBNull(4) ? null : reader.GetString(3), //Handles NULL VALUES
+                                Email = reader.IsDBNull(5) ? null : reader.GetString(4),
+                            };
+                            technicians.Add(technician);
+                        }
+                    }
+                }
+            }
+            return technicians;
+        }
     }
 }
